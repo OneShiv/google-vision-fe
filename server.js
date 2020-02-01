e01e26e0084463363475b52421aa841dca45a76e
oneshiv <shivshankar_gulab.shejwal@syngenta.com>
2020-02-01 15:32:23 +0530

    added server file so build can be deplyed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = 3006;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+});
